5491124c03ed988a9d479511f74b467febc6186e
Rafael Chavez <rchavez@itexico.net>
2017-04-04 08:38:03 -0500

    P-1

--- APM - Start/app/app.module.ts
+++ APM - Start/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import {ProductListComponent} from './products/product-list.component';
 import { AppComponent }  from './app.component';
 import {FormsModule} from '@angular/forms';
+import {ProductFilterPipe} from './products/product-filter.pipe';
+import {StarComponent} from './shared/star.component';
+import {HttpModule} from '@angular/http';
+import {ProductDetailComponent} from './products/product-detail.component';
+import {WelcomeComponent} from './home/welcome.component'
+import {RouterModule} from '@angular/router'
 
 @NgModule({
-  imports: [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, ProductListComponent ],
+  imports: [ BrowserModule, FormsModule, HttpModule, RouterModule],
+  declarations: [ AppComponent, ProductListComponent, ProductFilterPipe, StarComponent, WelcomeComponent, ProductDetailComponent],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
--- APM - Start/app/products/product-list.component.ts
+++ APM - Start/app/products/product-list.component.ts
@@ -1,47 +1,40 @@
-import {Component} from '@angular/core'
+import {Component, OnInit} from '@angular/core'
+import {IProduct} from './product'
+import {ProductService} from './product.service'
 
 @Component({
     selector: "pm-products",
-    templateUrl: 'app/products/product-list.component.html'
+    moduleId: module.id,
+    templateUrl: 'product-list.component.html',
+    styleUrls: ['product-list.component.css']
 })
-export class ProductListComponent{
+export class ProductListComponent implements OnInit {
     pageTitle: string = "Product List"
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    listFilter: string = 'cart';
-    products: any[] = [{
-        "productId": 2,
-        "productName": "Garden Cart",
-        "productCode": "GDN-0023",
-        "releaseDate": "March 18, 2016",
-        "description": "15 gallon capacity rolling garden cart",
-        "price": 32.99,
-        "starRating": 4.2,
-        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-    },
-    {
-        "productId": 5,
-        "productName": "Hammer",
-        "productCode": "TBX-0048",
-        "releaseDate": "May 21, 2016",
-        "description": "Curved claw steel hammer",
-        "price": 8.9,
-        "starRating": 4.8,
-        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
-    },
-    {
-        "productId": 8,
-        "productName": "Saw",
-        "productCode": "TBX-0022",
-        "releaseDate": "May 15, 2016",
-        "description": "15-inch steel blade hand saw",
-        "price": 11.55,
-        "starRating": 3.7,
-        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png"
-    }];
+    listFilter: string;
+    errorMessage: string;
+    products: IProduct[];
+
+    constructor(private _productService: ProductService){
+    }
 
     toggleImage(): void {
         this.showImage = !this.showImage;
+    }
+
+    clear(): void{
+        this.listFilter = '';
+    }
+
+    ngOnInit(): void {
+        this._productService.getProducts()
+        .subscribe(products => this.products = products,
+                   error => this.errorMessage = <any>error);
+    }
+
+    onRatingClicked(message: string): void {
+        this.pageTitle = "Product List: " + message;
     }
 }
